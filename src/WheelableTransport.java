d4afa822dae323029bb937be5e1e1c9a7a2af59f
User <xtoycohcohu@mail.ru>
2024-04-09 12:10:29 +0700

    Что у меня не так с классом велосипед?

---
+++ src/WheelableTransport.java
@@ -0,0 +1,28 @@
+public abstract class WheelableTransport {
+    private String modelName;
+    private int wheelsCount;
+
+    public WheelableTransport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+    public abstract void check();
+    public abstract void updateTyre();
+}
+
